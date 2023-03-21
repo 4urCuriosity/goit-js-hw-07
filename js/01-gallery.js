43ac1c4c333090f45f1587933d4642d4307f2ce6
TurtlePower <agnkvdm@gmail.com>
2023-03-21 23:57:05 +0200

    add gallery markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryListEl = document.querySelector('ul.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems).join('');
+
+galleryListEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(photos) {
+  return photos.map(({ preview, original, description }) => {
+    return `
+		<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+		`;
+  });
+}
