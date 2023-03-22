4fb9af870cf0d45a2afa43a00abe886d08806f4b
TurtlePower <agnkvdm@gmail.com>
2023-03-22 14:02:13 +0200

    add finished second gallery task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector('ul.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems).join('');
+
+galleryListEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const lightBoxGallery = new SimpleLightbox('ul.gallery li a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createGalleryMarkup(photos) {
+  return photos.map(({ preview, original, description }) => {
+    return `
+		<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>
+		`;
+  });
+}
