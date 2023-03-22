f9eaeb7407ce99775f755d4ef9c12481a2551e90
TurtlePower <agnkvdm@gmail.com>
2023-03-22 12:57:50 +0200

    add 1st hw task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,12 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
 const galleryListEl = document.querySelector('ul.gallery');
 const galleryMarkup = createGalleryMarkup(galleryItems).join('');
 
 galleryListEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryListEl.addEventListener('click', onPhotoLinkClick);
 
 function createGalleryMarkup(photos) {
   return photos.map(({ preview, original, description }) => {
@@ -22,4 +23,30 @@ function createGalleryMarkup(photos) {
 </li>
 		`;
   });
+}
+
+function onPhotoLinkClick(e) {
+  e.preventDefault();
+
+  const activePhoto = e.target;
+  const largeModalPhoto = basicLightbox.create(
+    `
+		<img width="1400" height="900" src="${activePhoto.dataset.source}">
+	`
+  );
+
+  largeModalPhoto.show();
+
+  if (largeModalPhoto.visible()) {
+    galleryListEl.addEventListener('keydown', onEscClose);
+  }
+
+  function onEscClose(e) {
+    if (e.code !== 'Escape') {
+      return;
+    }
+
+    largeModalPhoto.close();
+    galleryListEl.removeEventListener('keydown', onEscClose);
+  }
 }
